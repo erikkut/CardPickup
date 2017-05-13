702f75869c2249c1b11b47ddfdf7bb1c726107cc
erikkut <eric.manuel.torres@gmail.com>
2017-05-12 21:36:14 -0600

    Some more work

--- FirstAgent_EricTorres.java
+++ FirstAgent_EricTorres.java
@@ -2,6 +2,7 @@ package CardPickup;
 
 import com.sun.org.apache.xerces.internal.xs.StringList;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -12,7 +13,7 @@ import java.util.*;
  * protected int currentNode; //Your current location
  * protected int oppNode; //Opponent's current position
  * protected Card oppLastCard;	//Opponent's last picked up card
- *
+ * <p>
  * Important methods inherited from Player Class:
  * Method that is used to determine if a move is valid. This method should be used to help players
  * determine if their actions are valid. GameMaster has a local copy of this method and all the
@@ -20,11 +21,10 @@ import java.util.*;
  * invalid action in to a "valid" one will not help you as the GameMaster will still see the action
  * as invalid.
  * protected boolean isValidAction(Action a); //This method can be used to determine if an action is valid
- *
+ * <p>
  * NOTE TO STUDENTS: The game master will only tell the player the results of your and your opponents actions.
  * It will not update your graph for you. That is something we left you to do so that you can update your
  * graphs, opponent hand, horoscope, etc. intelligently and however you like.
-
  *
  * @author Marcus Gutierrez
  * @version 04/15/2015
@@ -33,7 +33,7 @@ import java.util.*;
 /**
  * Modified by Eric on 4/27/2017.
  */
-public class FirstAgent_EricTorres extends Player{
+public class FirstAgent_EricTorres extends Player {
     protected final String newName = "NotDumb"; //Overwrite this variable in your player subclass
 
     //Keep the list of known cards
@@ -41,6 +41,9 @@ public class FirstAgent_EricTorres extends Player{
 
     //Keep the list of possible cards left
     float[] estValues;
+
+    //Ordered nodes. Array of ordered nodes
+    ArrayList<HeuristicNode> sorted;
 
     HandEvaluator evaluator = new HandEvaluator();
 
@@ -60,9 +63,12 @@ public class FirstAgent_EricTorres extends Player{
         //Initialize node values
         estValues = new float[graph.length];
 
+        //Initialize sorted list of nodes by estimate value
+        sorted = new ArrayList<>();
+
         //Set known
         known = new ArrayList<>();
-        for(int i = 0; i < hand.getNumHole(); i++){
+        for (int i = 0; i < hand.getNumHole(); i++) {
             known.add(hand.getHoleCard(i));
         }
 
@@ -76,8 +82,9 @@ public class FirstAgent_EricTorres extends Player{
 //        printIntel();
 
         updateKnownAndGraphAndValues();
-
-//        printIntel();
+        if(isInTop(graph[11], 1))
+            System.out.println("YES THIS IS TOP:");
+        printIntel();
     }
 
     /**
@@ -89,15 +96,15 @@ public class FirstAgent_EricTorres extends Player{
      * @param opponentPickedUp Notifies if the opponent picked up a card last turn
      * @param c The card that the opponent picked up, if any (null if the opponent did not pick up a card)
      */
-    protected void opponentAction(int opponentNode, boolean opponentPickedUp, Card c){
+    protected void opponentAction(int opponentNode, boolean opponentPickedUp, Card c) {
         oppNode = opponentNode;
-        if(opponentPickedUp)
+        if (opponentPickedUp)
             oppLastCard = c;
         else
             oppLastCard = null;
 
         //Add the picked up card to known and update graph and est values
-        if(c != null) {
+        if (c != null) {
             known.add(c);
             graph[opponentNode].clearPossibleCards();
             updateKnownAndGraphAndValues();
@@ -111,13 +118,13 @@ public class FirstAgent_EricTorres extends Player{
      * @param currentNode Opponent's current location
      * @param c The card that you picked up, if any (null if you did not pick up a card)
      */
-    protected void actionResult(int currentNode, Card c){
+    protected void actionResult(int currentNode, Card c) {
         this.currentNode = currentNode;
-        if(c != null)
+        if (c != null)
             addCardToHand(c);
 
         //Add the picked up card to known and update graph and est values
-        if(c != null) {
+        if (c != null) {
             known.add(c);
             graph[currentNode].clearPossibleCards();
             updateKnownAndGraphAndValues();
@@ -128,75 +135,49 @@ public class FirstAgent_EricTorres extends Player{
      * Player logic goes here
      */
     public Action makeAction() {
-        printIntel();
-        /*Random r = new Random();
-        int neighbor;
-        if (graph[currentNode].getNeighborAmount()==1)
-            neighbor = graph[currentNode].getNeighbor(0).getNodeID();
-        else
-            neighbor = graph[currentNode].getNeighbor(r.nextInt(graph[currentNode].getNeighborAmount())).getNodeID();
-        return new Action(ActionType.PICKUP, neighbor);*/
+
+
+//        printIntel();
 
         //If running out of turns just pick up
-        if(turnsRemaining+200 <= hand.getNumUp()){
-            return new Action(ActionType.PICKUP, randomNodeId());
-        }
-
-        //Get top 6 nodes
-
-        int bestIndex = -1;
-        int betterIndex = -1;
-        int okayIndex = -1;
-        int mediumIndex = -1;
-        int worseIndex = -1;
-        int worstIndex = -1;
-        float compare = -100.0f;
-        for(int i = 0; i < graph.length; i++){
-            if(estValues[i] > compare){
-                compare = estValues[i];
-                worstIndex = worseIndex;
-                worseIndex = mediumIndex;
-                mediumIndex = okayIndex;
-                okayIndex = betterIndex;
-                betterIndex = bestIndex;
-                bestIndex = i;
-            }
-        }
-
-        //If current node is within top 2 highest estimates values, pick it up. Otherwise, move towards best.
-        if(currentNode == bestIndex || betterIndex == currentNode || okayIndex == currentNode || mediumIndex == currentNode || worseIndex == currentNode || worstIndex == currentNode){
+        if (turnsRemaining <= 5 - hand.getNumHole() + hand.getNumUp()) {
             return new Action(ActionType.PICKUP, graph[currentNode].getNodeID());
         }
 
-        //move to a random node
-        return new Action(ActionType.MOVE, randomNodeId());
-//        return new Action(ActionType.MOVE, findPathToNode(bestIndex));
+        //If current node is within top 2 highest estimates values, pick it up. Otherwise, move towards best.
+        if (isInTop(graph[currentNode], 2)) {
+            return new Action(ActionType.PICKUP, graph[currentNode].getNodeID());
+        }
+
+        //move to best node
+//        return new Action(ActionType.MOVE, randomNodeId());
+        int indexOfBestNode = sorted.get(0).node.getNodeID();
+        return new Action(ActionType.MOVE, indexOfBestNode);
     }
 
     /**
      * Update the known card list, and the possible cards of each node
      */
-    public void updateKnownAndGraphAndValues(){
+    public void updateKnownAndGraphAndValues() {
         boolean reRun = false;
 
         //traverse entire graph, remove impossible cards from each node
-        for(int nodeIndex = 0; nodeIndex < graph.length; nodeIndex++) {
+        for (int nodeIndex = 0; nodeIndex < graph.length; nodeIndex++) {
             ArrayList<Card> cardsInNode = graph[nodeIndex].getPossibleCards();
 
             //Update the estimated value of picking up this node
-            if(cardsInNode.size() == 0){
+            if (cardsInNode.size() == 0) {
                 //no value since can't pickup
                 estValues[nodeIndex] = 0.0f;
-            }
-            else{
+            } else {
                 float prob = 1.0f / cardsInNode.size();
                 float value = 0.0f;
                 Hand ghostHand = new Hand();
 
-                for(int i = 0; i < cardsInNode.size(); i++){
+                for (int i = 0; i < cardsInNode.size(); i++) {
 
                     //Add hole cards on every estimation
-                    for(int j = 0; j < hand.getNumHole(); j++){
+                    for (int j = 0; j < hand.getNumHole(); j++) {
                         ghostHand.addHoleCard(hand.getHoleCard(j));
                     }
 
@@ -211,7 +192,7 @@ public class FirstAgent_EricTorres extends Player{
                 estValues[nodeIndex] = value;
             }
 
-            if(cardsInNode.size() == 1) {
+            if (cardsInNode.size() == 1) {
                 //node is known, skip this node
                 continue;
             }
@@ -235,22 +216,29 @@ public class FirstAgent_EricTorres extends Player{
             }
         }
 
-        if(reRun)
+        if (reRun)
             updateKnownAndGraphAndValues();
+
+        //Sort the finished estimate values
+        sorted.clear();
+        for(int i = 0; i < estValues.length; i++){
+            sorted.add(new HeuristicNode(graph[i], estValues[i]));
+        }
+        Collections.sort(sorted, new HeuristicNode(null, 0));
     }
 
     /**
      * Returns anti-intersection of two Card ArrayLists (The cards that don't match)
      */
-    public ArrayList<Card> containsKnown(ArrayList<Card> known, ArrayList<Card> node){
+    public ArrayList<Card> containsKnown(ArrayList<Card> known, ArrayList<Card> node) {
         ArrayList<Card> difference = (ArrayList<Card>) node.clone();
 
         //For every known card
-        for(int i = 0; i < known.size(); i++){
+        for (int i = 0; i < known.size(); i++) {
 
             //for every possible card
-            for(int j = 0; j < node.size(); j++){
-                if(known.get(i).shortName().equalsIgnoreCase(node.get(j).shortName())){
+            for (int j = 0; j < node.size(); j++) {
+                if (known.get(i).shortName().equalsIgnoreCase(node.get(j).shortName())) {
                     //remove
                     difference.remove(j);
                     continue;
@@ -264,33 +252,53 @@ public class FirstAgent_EricTorres extends Player{
     /**
      * Prints current player known cards and graph
      */
-    public void printIntel(){
+    public void printIntel() {
         //print known
         System.out.print("Known cards: ");
-        for(int i = 0; i < known.size(); i++){
-            System.out.print(known.get(i).shortName()+", ");
+        for (int i = 0; i < known.size(); i++) {
+            System.out.print(known.get(i).shortName() + ", ");
         }
         System.out.println("");
 
         //print graph
-        for(int i = 0; i < graph.length; i++){
-            System.out.print("Node: "+i+"  Est. Val: "+estValues[i]+", ");
+        for (int i = 0; i < graph.length; i++) {
+            System.out.print("Node: " + i + "  Est. Val: " + estValues[i] + ", ");
 
             ArrayList<Card> possible = graph[i].getPossibleCards();
-            for(int j = 0; j < possible.size(); j++){
-                System.out.print(possible.get(j).shortName()+", ");
+            for (int j = 0; j < possible.size(); j++) {
+                System.out.print(possible.get(j).shortName() + ", ");
             }
 
             System.out.println("");
         }
 
-        System.out.println("");
+        System.out.print("Sorted List: ");
+
+        //print sorted
+        for (HeuristicNode i: sorted) {
+            System.out.print(i.node.getNodeID()+", ");
+        }
+        System.out.println("\n");
     }
 
-    public int randomNodeId(){
+    /**
+     * Returns if a node is within top estimated values (number given as parameter)
+     */
+    public boolean isInTop(Node any, int top){
+        for(HeuristicNode i: sorted){
+            if(top < 0)
+                break;
+            if(i.node.getNodeID() == any.getNodeID())
+                return true;
+            top--;
+        }
+        return false;
+    }
+
+    public int randomNodeId() {
         Random r = new Random();
         int rInd = r.nextInt(graph.length);
-        while(!graph[currentNode].getNeighborList().contains(graph[rInd]) && estValues[rInd] > 0.0){
+        while (estValues[rInd] == 0.0 && currentNode == rInd) {
             rInd = r.nextInt(graph.length);
         }
 
@@ -299,68 +307,184 @@ public class FirstAgent_EricTorres extends Player{
 
     /**
      * TODO: Fix bugs to implement
-     * @param index: index of node to go to
+     * @param findMe: index of node to go to
      * @return nodeID of node to move to in order to get to wanted node.
      */
-    public int findPathToNode(int index){
-        ArrayList<Node> neighbors = graph[currentNode].getNeighborList();
+    public HeuristicNode findPathToNode(int findMe) {
+        PriorityQueue visited = new PriorityQueue();
+        PriorityQueue fringe = new PriorityQueue();
+        HeuristicNode start = new HeuristicNode(graph[currentNode], 0);
+        fringe.enqueue(start);
+        visited.enqueue(start);
 
-        /*
-        //Just move to node if its within neighbors
-        if(neighbors.contains(graph[index])){
-            return graph[index].getNodeID();
+        HeuristicNode current;
+        HeuristicNode solution = null;
+
+        //while fringe isn't empty
+        while (!fringe.isEmpty()) {
+            current = fringe.dequeue();
+            System.out.println("Current: "+current.node.getNodeID());
+            System.out.println("Current: "+System.currentTimeMillis());
+
+            //changed to 'continue if solution has higher cost than other evaluated solutions'
+            //instead of just 'if goal node found, return it'
+            if (current.node.getNodeID() == graph[findMe].getNodeID()) {
+                if (current.val < fringe.peekEvaluation())
+                    return current;
+                else
+                    solution = current;
+            }
+
+            //get children of the current node
+            ArrayList<Node> ghostChildren = graph[currentNode].getNeighborList();
+
+            //check current node's children and add to queue
+            for (Node temp: ghostChildren) {
+                HeuristicNode temp2 = new HeuristicNode(temp, 1);
+
+                if(!visited.hasNode(temp2)){
+
+                    visited.enqueue(temp2);
+
+                    temp2.parent = current;
+
+                    //add up cost of nodes up to this node cumulatively
+                    temp2.val += current.val;
+                    //add heuristic to child Node before the priority enqueue
+                    temp2.f = 0;
+
+                    fringe.enqueue(temp2);
+                }
+
+            }
         }
-        else {
-            ArrayList<Node> visited = new ArrayList<>();
-            for(int i = 0; i < graph[currentNode].getNeighborAmount(); i++){
 
+        //no solution found
+        return solution;
+    }
+
+    public class HeuristicNode implements Comparator<HeuristicNode>{
+        //data
+        Node node;
+        float val; //cost of node
+        double f; //heuristic estimate
+
+        //fringe part
+        HeuristicNode next;
+        HeuristicNode parent;
+
+        public HeuristicNode(Node node, float val) {
+            this.node = node;
+            this.val = val;
+            this.f = 0;
+
+            next = null;
+            parent = null;
+        }
+
+        @Override
+        public int compare(HeuristicNode x1, HeuristicNode x2){
+            return (int) x2.val - (int) x1.val;
+        }
+    }
+
+    public class PriorityQueue {
+        private HeuristicNode head; //head will always be smallest node
+
+        public PriorityQueue(){
+            head = null;
+        }
+
+        public double peekEvaluation(){
+            if(isEmpty())
+                return 0;
+            return (head.val+head.f);
+        }
+
+        public void enqueue(HeuristicNode ghost){
+            if(isEmpty()){
+                head = ghost;
+                return;
             }
 
-        }*/
+            HeuristicNode temp = head;
+            HeuristicNode previous = null;
 
-
-        ArrayList<Node> visited = new ArrayList<>();
-        Map<Node, Node> prev = new HashMap<>();
-
-        Node curr = graph[currentNode];
-        LinkedList<Node> q = new LinkedList<>();
-        q.add(curr);
-        visited.add(curr);
-
-        while(!q.isEmpty()){
-            curr = q.remove();
-
-            //Found node, return path
-            if(curr.getNodeID() == graph[index].getNodeID()){
-                break;
+            //if its smaller than the head
+            if((ghost.f+ghost.val) < (temp.val+temp.f)){
+                ghost.next = head;
+                head = ghost;
+                return;
             }
-            else{
-                for(Node node : curr.getNeighborList()){
-                    boolean addVis = true;
-                    for(Node vis : visited){
-                        if(vis.getNodeID() == node.getNodeID()){
-                            addVis = false;
-                        }
-                    }
-                    if(addVis) {
-                        q.add(node);
-                        visited.add(node);
-                        prev.put(node, curr);
-                    }
-                    /*if(!visited.contains(node)){
-                        q.add(node);
-                        visited.add(node);
-                        prev.put(node, curr);
-                    }*/
+
+            //otherwise iterate until a spot for the Node is found
+            while(temp.next != null){
+                previous = temp;
+                temp = temp.next;
+
+                if((ghost.val+ghost.f) < (temp.val+temp.f)){
+                    previous.next = ghost;
+                    ghost.next = temp;
+                    return;
                 }
             }
+
+            temp.next = ghost;
         }
 
-        ArrayList<Node> path = new ArrayList<>();
-        for(Node node = graph[index]; node != null; node = prev.get(node)){
-            path.add(node);
-        }
-        return path.get(path.size()-1).getNodeID();
+        public HeuristicNode dequeue(){
+            if(isEmpty())
+                return null;
 
+            HeuristicNode temp = head;
+            if(head.next == null){
+                head = null;
+                return temp;
+            }
+
+            head = head.next;
+            return temp;
+        }
+
+        public boolean isEmpty(){
+            return head == null;
+        }
+
+        public void printQueue(){
+            HeuristicNode temp = head;
+            while(temp != null){
+                System.out.println(""+(temp.val+temp.f));
+                temp = temp.next;
+            }
+        }
+
+        public boolean hasNode(HeuristicNode ghost){
+            if(head == null)
+                return false;
+
+            HeuristicNode temp = head;
+            while(temp != null){
+                if(temp.node.getNodeID() == ghost.node.getNodeID())
+                    return true;
+                temp = temp.next;
+            }
+            return false;
+        }
+
+        public int getSize(){
+            if(isEmpty())
+                return 0;
+
+            HeuristicNode temp = head;
+            int count = 0;
+            while(temp != null){
+                count++;
+                temp = temp.next;
+            }
+
+            return count;
+        }
     }
+
+
 }
